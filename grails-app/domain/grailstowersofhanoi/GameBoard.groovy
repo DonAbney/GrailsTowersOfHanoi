0bafc2ffa88684a286497c412a4b8b519436a866
Christopher Vaughn <vaughncm@gmail.com>
2014-02-05 20:10:19 -0500

    Fixing gameboard logic

--- grails-app/domain/grailstowersofhanoi/GameBoard.groovy
+++ grails-app/domain/grailstowersofhanoi/GameBoard.groovy
@@ -1,12 +1,13 @@
-package org.gna
+package grailstowersofhanoi
 
 class GameBoard {
-    
-    List towers
 
-    GameBoard() {
-      towers[0] = new Peg([new Ring(1), new Ring(2), new Ring(3)])
-      towers[1] = new Peg()
-      towers[2] = new Peg()
-    }
+  List towers = []
+
+  def GameBoard() {
+    towers[0] = new Peg([new Ring(1), new Ring(2), new Ring(3)])
+    towers[1] = new Peg()
+    towers[2] = new Peg()
+  }
+
 }
