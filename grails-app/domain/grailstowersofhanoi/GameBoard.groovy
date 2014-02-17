6fc807be7ee2b843259cf51252e8a975e8a045fd
Don Abney <tdonaldabney@gmail.com>
2014-02-16 20:30:14 -0500

    initializing to a proper starting state

--- grails-app/domain/grailstowersofhanoi/GameBoard.groovy
+++ grails-app/domain/grailstowersofhanoi/GameBoard.groovy
@@ -5,8 +5,8 @@ class GameBoard {
 	List towers = []
 
 	def GameBoard() {
-		towers[0] = new Peg([new Ring(1), new Ring(3)])
-		towers[1] = new Peg([new Ring(2)])
+		towers[0] = new Peg([new Ring(1),new Ring(2), new Ring(3)])
+		towers[1] = new Peg([])
 		towers[2] = new Peg([])
 	}
 
